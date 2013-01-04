a11497abcdabe7dc8e38eb652d2160dbf673151f
Tim Poulsen <tim@skypanther.com>
2013-01-04 15:38:47 -0500

    Added javascript lesson, rearranged the others

---
+++ 2_javascript/i18n/localizations.js
@@ -0,0 +1,19 @@
+/*
+	Localization strings file. To translate, copy the entire "en-US" branch, paste in as new member of the object, 
+	set its name to be the two-letter language code, and translate each string.
+	As shown, strings can include simple HTML.
+*/
+String.toLocaleString({
+	"en-US": {
+		"%l_filetitle": "JavaScript",
+		"%l_lessontitle": "JavaScript",
+		"%l_subtitle": "Titanium Certified Developer (TCD) Training",
+		"%l_slidenote_titleslide": "",
+		"%l_qa_slide": "Q&amp;A",
+		"%l_slidenote_qa": ""
+	},
+	"ja": {
+		"%l_filetitle": "はじめに",
+		"%l_lessontitle": "はじめに",
+	}
+});
