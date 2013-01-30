6428e563824086619f12c37ebe44c58cac50a928
Tim Poulsen <tim@skypanther.com>
2013-01-30 16:57:43 -0500

    corrections/changes for 8,9, 10, the rearrange to add in ACS lesson

---
+++ 11_acs/i18n/localizations.js
@@ -0,0 +1,14 @@
+/*
+	Localization strings file. To translate, copy the entire "en-US" branch, paste in as new member of the object, 
+	set its name to be the two-letter language code, and translate each string.
+	As shown, strings can include simple HTML.
+*/
+String.toLocaleString({
+	"en-US": {
+		"%l_filetitle": "LessonTitle",
+		"%l_lessontitle": "LessonTitle",
+		"%l_subtitle": "Titanium Certified Developer (TCD) Training",
+		"%l_slidenote_titleslide": "",
+		"%l_agenda_title": "In this lesson, you will: "
+	}
+});
