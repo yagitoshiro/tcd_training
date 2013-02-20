b2ed51f1779f0746152f836377b67a8d52fc344a
Tim Poulsen <tim@skypanther.com>
2013-02-20 11:40:50 -0800

    remove finished code from starting file

--- labcode/Starting/12_webcontent/app/controllers/entryView.js
+++ labcode/Starting/12_webcontent/app/controllers/entryView.js
@@ -1,6 +1,5 @@
 var updateWebView = function() {
 	$.a.blur();$.b.blur();
-	Ti.App.fireEvent('updateGraph', {a: Math.floor($.a.value), b: Math.floor($.b.value), c: Math.floor($.c.value)});
 };
 var calcC = function() {
 	$.c.value = String(Math.round(100 - $.a.value - $.b.value));
