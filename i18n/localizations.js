e05d1e9b43ffbc8b8b256e8b5d29e3696b97351a
Toshiro Yagi <yagitoshiro@gmail.com>
2013-04-09 15:49:49 +0900

    localized top page

--- i18n/localizations.js
+++ i18n/localizations.js
@@ -7,7 +7,30 @@ String.toLocaleString({
 	"en-US": {
 		"%l_welcome": "Welcome To",
 		"%l_coursename": "Titanium Certified Developer (TCD) Training",
-		"%l_agenda_title": "AGENDA",
+		"%l_agenda_title": "AGENDA"
 	},
-
+  "ja": {
+		"%l_welcome": "ようこそ",
+		"%l_coursename": "Titanium Certified Developer (TCD) Training",
+		"%l_agenda_title": "アジェンダ",
+    "%l_agenda1_title": "アジェンダ 一日目",
+    "%l_courseintro": "コース紹介",
+    "%l_lesson1": "はじめに",
+    "%l_lesson2": "Titaniumの基本",
+    "%l_lesson3": "JavaScript",
+    "%l_lesson4": "ユーザーインターフェース",
+    "%l_lesson5": "Alloyの概要",
+    "%l_agenda2_title": "アジェンダ 二日目",
+    "%l_lesson6": "モデル＆コレクション",
+    "%l_lesson7": "ネットワーク",
+    "%l_lesson8": "マルチメディア",
+    "%l_lesson9": "ファイルシステム",
+    "%l_lesson10": "位置情報と地図",
+    "%l_agenda3_title": "アジェンダ 三日目",
+    "%l_lesson11": "Appcelerator Cloud Services",
+    "%l_lesson12": "ウェブコンテンツ",
+    "%l_lesson13": "デバッグとツール",
+    "%l_lesson14": "実機テスト",
+    "%l_lesson15": "アプリの公開"
+  }
 });
